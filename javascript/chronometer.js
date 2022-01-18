ecd8527391787acac325a1154dac1f3288dc0412
Vitor Santos <vitor93gs@gmail.com>
2022-01-18 17:30:36 -0300

    iteracao1 feita

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,51 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
-  }
-
+    this.currentTime = 0;
+    this.intervalId = null;    
+  }   
   start(callback) {
-    // ... your code goes here
+    
+    if(!callback===true){
+      this.intervalId = setInterval(() => { 
+        this.currentTime ++
+      }, 1000);
+    }
+    else{
+      this.intervalId = setInterval((callback) => { 
+        callback(); 
+        this.currentTime ++
+      }, 1000)
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime - (Math.floor(this.currentTime/60)*60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value <= 9){
+      value = `0${value}`
+    }
+    else{
+      value = `${value}`
+    }
+    return value
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
